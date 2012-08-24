391928acd0fcae4886828ace075f80e4fc52842f
Sebastien Gignoux <sgignoux@gmail.com>
2012-08-24 14:42:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MockupGo::Application.config.secret_token = 'e79bfd7f18eb271b79e11a59a130c50ec9134b28b57028ad5cac54b213d2cee4a1fb5dc96b2910234e06e155698cb195de6388c0ee36b2243b16c358a7079129'
