7f914ea8ee147eeb6ab081d1445bb55e4f7057d2
Sebastien Gignoux <sgignoux@gmail.com>
2012-09-04 20:27:43 +0200

    Added dashboard links to application template and increased coverage of StaticPagesController test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,11 +4,38 @@ describe StaticPagesController do
   describe "GET dashboard" do
      
     context "when signed in" do
-      it "should show only the signed in user's projects"
+      let(:user)         { FactoryGirl.create(:user, email: "first@example.com") }
+      let(:another_user) { FactoryGirl.create(:user, email: "second@example.com") }
+
+      before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        sign_in user  
+      end
+
+      let!(:p1) { FactoryGirl.create(:project, user: user, name: "project foo") }
+      let!(:p2) { FactoryGirl.create(:project, user: user, name: "project bar") }
+
+      let!(:q1) { FactoryGirl.create(:project, user: another_user, name: "other user project foo") }
+      let!(:q2) { FactoryGirl.create(:project, user: another_user, name: "other user project bar") }
+
+      it "should show the signed in user's projects" do
+        get :dashboard
+        assigns(:projects).should include(p1)
+        assigns(:projects).should include(p2)
+      end
+
+      it "should should not show other user's projects" do
+        get :dashboard
+        assigns(:projects).should_not include(q1)
+        assigns(:projects).should_not include(q2)
+      end
     end
 
     context "when not signed in" do
-      it "should redirect to the sign in page"
+      it "should redirect to the sign in page" do
+        get :dashboard
+        response.should redirect_to new_user_session_path
+      end
     end
   end
 end
--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -2,31 +2,38 @@ require "spec_helper"
 
 describe "layouts/application.html.erb" do
 
-  context "there is an flash[:notice]" do
-    let(:flash_msg) { "This is a flash notice" }
-    before do
-      @view.should_receive(:user_signed_in?).and_return(true)
-      flash[:notice] = flash_msg
+  context "signed in user" do
+
+    before { @view.should_receive(:user_signed_in?).and_return(true) }
+
+    it "should contain a link to the dashboard" do
       render
+      rendered.should have_css("a[href=\"#{dashboard_path}\"]")
     end
 
-    it "display the flash messages" do
-      rendered.should have_content(flash_msg)
+    context "there is an flash[:notice]" do
+      let(:flash_msg) { "This is a flash notice" }
+      before do
+        flash[:notice] = flash_msg
+        render
+      end
+
+      it "display the flash messages" do
+        rendered.should have_content(flash_msg)
+      end
     end
+
+    context "there is an flash[:alert]" do
+      let(:flash_msg) { "This is a flash notice" }
+      before do
+        flash[:alert] = flash_msg
+        render
+      end
+
+      it "display the flash messages" do
+        rendered.should have_content(flash_msg)
+      end
+    end
+
   end
-
-  context "there is an flash[:alert]" do
-    let(:flash_msg) { "This is a flash notice" }
-    before do
-      @view.should_receive(:user_signed_in?).and_return(true)
-      flash[:alert] = flash_msg
-      render
-    end
-
-    it "display the flash messages" do
-      rendered.should have_content(flash_msg)
-    end
-  end
-
-
 end
