95032850828f0942497f179a66929130dc258a6d
Sebastien Gignoux <sgignoux@gmail.com>
2012-09-29 17:57:11 +0200

    add multiple file upload with jquery-upload. It is still very basic

--- app/controllers/image_mockups_controller.rb
+++ app/controllers/image_mockups_controller.rb
@@ -1,14 +1,7 @@
 class ImageMockupsController < ApplicationController
 
   def create
-    puts params.inspect
-    @mockup = ImageMockup.new(params[:image_mockup])
-    if @mockup.save
-      flash[:notice] = "Successfully created mockup."
-      redirect_to [@mockup.page.project, @mockup.page]
-    else
-      render :action => 'new'
-    end
+    @mockup = ImageMockup.create(params[:image_mockup])
   end
 
 end
