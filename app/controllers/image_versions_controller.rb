79a76e931ef882b7f3c19b40bffa823006ad9971
Sebastien Gignoux <sgignoux@gmail.com>
2012-10-25 00:38:53 +0200

    feature: show automatically help for preview page only once per session

--- app/controllers/image_versions_controller.rb
+++ app/controllers/image_versions_controller.rb
@@ -6,6 +6,12 @@ class ImageVersionsController < ApplicationController
 
   def preview
     @image_version = ImageVersion.find(params[:id])
+
+    if session[:display_help_first_session_load] != true
+      session[:display_help_first_session_load] = true
+      @dislay_help_modal_on_load = true
+    end
+
   end
 
 end
