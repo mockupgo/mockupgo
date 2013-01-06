9ddf96c885775115483af5c697bfb9dbcdfc141d
Sebastien Gignoux <sgignoux@gmail.com>
2013-01-06 20:55:43 +0100

    fix: use correct doamine name for email sending config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,6 @@ MockupGo::Application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = {host: "immense-escarpment-7235.herokuapp.com"}
+  config.action_mailer.default_url_options = {host: "mockupgo.herokuapp.com"}
 
 end
