e709f8eadb8764bd97009442dc69ccc98d6fcf9b
Sebastien Gignoux <sgignoux@gmail.com>
2013-01-08 00:33:36 +0100

    feature: display connections for real time exchange

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -3,10 +3,16 @@ class PusherController < ApplicationController
   
   def auth
     if current_user
-      response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+      response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
+        :user_id => current_user.id, # => required
+        :user_info => { # => optional - for example
+          :email => current_user.email
+        }
+      })
       render :json => response
     else
       render :text => "Forbidden", :status => '403'
     end
   end
 end
+
