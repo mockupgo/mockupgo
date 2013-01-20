4d26f932dd9769513328efba3c3c29c5fcfb76eb
Sebastien Gignoux <sgignoux@gmail.com>
2013-01-20 22:27:19 +0100

    feature: add name of user when signed in, improve presentation of sign in and sign up screen, clean up create project block in dashboard

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -10,7 +10,7 @@ end
 
 When /^the user create a new project$/ do
   # click_button 'New project'
-  fill_in "Name",    :with => 'new project name'
+  fill_in "Project name",    :with => 'new project name'
   click_button 'Create Project'
 end
 
